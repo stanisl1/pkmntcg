31da412942c8d844c883390afbef6ead3677105e
Jeff Stanislaw <stanisl1@illinois.edu>
2013-05-18 17:45:47 -0500

    initial commit:

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pkmntcg::Application.config.secret_token = '1543e72e820e122a3072da263a17f4e3f583d0f941c7fdcb0b4ce1542d52025f1e521d70a4f76f9a09d9b84023e50dddbddcfb1fe66620bde16012966bcb9e7b'
